c03de34210ba676bdf51841000d6c0d6f0d469ba
Dmitry Shelomanov <userbq201@mail.ru>
2019-10-07 23:13:07 +0300

    chore: add docs

---
+++ docs/precache-manifest.bd6682a9c7880aa9c5dadae05bebc566.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378dd2d0ba32821813d03d2f2f8ddd27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cb8a3450af418d357d3",
+    "url": "/static/js/2.8f32d1d4.chunk.js"
+  },
+  {
+    "revision": "d7c90f5af47f1f8af693",
+    "url": "/static/js/main.cc4d58a5.chunk.js"
+  },
+  {
+    "revision": "45fb0354d2015560e4ad",
+    "url": "/static/js/runtime-main.19d2d708.js"
+  }
+]);
