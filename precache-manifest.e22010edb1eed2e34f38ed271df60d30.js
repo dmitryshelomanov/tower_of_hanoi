8bd084e664db018160703bebf5ad64009f8508f8
Dmitry Shelomanov <userbq201@mail.ru>
2019-10-08 00:35:30 +0300

    Updates

---
+++ precache-manifest.e22010edb1eed2e34f38ed271df60d30.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730f33e8d7e1418918533e3a3351d058",
+    "url": "/tower_of_hanoi/index.html"
+  },
+  {
+    "revision": "08c6d33bf3c565c9d5b1",
+    "url": "/tower_of_hanoi/static/js/2.cc4c9cd8.chunk.js"
+  },
+  {
+    "revision": "cfc74611eadaee502752",
+    "url": "/tower_of_hanoi/static/js/main.e7418e74.chunk.js"
+  },
+  {
+    "revision": "1451e55ff9dec4737b0d",
+    "url": "/tower_of_hanoi/static/js/runtime-main.9bbfff6f.js"
+  }
+]);
